2f1ae02408071ef0098183613768c6bb40520f4a
bugg86 <jackfink68@yahoo.com>
2026-02-20 21:02:22 -0500

    start svelte project

--- usav3dans/Services/Interfaces/IGoogleService.cs
+++
@@ -1,9 +0,0 @@
-using Google.Apis.Sheets.v4.Data;
-
-namespace usav3dans.Services.Interfaces;
-
-public interface IGoogleService
-{
-    Task<ValueRange> PushDansMp(string mp, string sheet);
-    Task<ValueRange> PushTryoutsMp(string mp);
-}
